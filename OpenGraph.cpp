4e49fced33416012ae5c834b79dac72ea8e2574f
Sitong An <ansitong@gmail.com>
2019-03-07 20:11:20 +0100

    build EdgeForward and EdgeBackward

--- OpenGraph.cpp
+++ OpenGraph.cpp
@@ -6,8 +6,10 @@
 #include <map>
 #include <tuple>
 #include <array>
+#include <set>
 #include <cstdint> //int64_t: used for ONNX IR
 #include <cstddef> // std::size_t
+#include "ROOT/RVec.hxx"
 
 
 #include "onnx.pb.h"
@@ -114,37 +116,106 @@ namespace OpenGraph{
    };
 
 
+
 }
 }
 }
 
 
+string print(const vector<std::int64_t>& vec, const onnx::GraphProto& graph){
+   string str {""};
+   for (auto const& item : vec){
+      str += std::to_string(item);
+      if (item == -1){
+         str += ("[I/O]");
+      }else{
+         str += ("[" + graph.node(item).op_type() + "]");
+      }
+      str += ",";
+   }
+   return str;
+}
 
 
 int main(){
-  GOOGLE_PROTOBUF_VERIFY_VERSION;
-  onnx::ModelProto model;
-  std::fstream input("LinearNN.onnx", std::ios::in | std::ios::binary);
-  if (!model.ParseFromIstream(&input)){
-    std::cerr << "Failed to parse onnx file." << endl;
-    return -1;
-  }
-  //model I/O
-  cout << "fIRVersion: " << model.ir_version() << endl;
-  cout << "fModelVersion:" << model.model_version() << endl;
-  cout << "opsetid_size: " << model.opset_import_size() << endl;
-  const onnx::OperatorSetIdProto& opset = model.opset_import(0);
-  cout << "Opset version: " << opset.version() << endl;
+   GOOGLE_PROTOBUF_VERIFY_VERSION;
+   onnx::ModelProto model;
+   std::fstream input("resnet18v1.onnx", std::ios::in | std::ios::binary);
+   if (!model.ParseFromIstream(&input)){
+      std::cerr << "Failed to parse onnx file." << endl;
+      return -1;
+   }
+   //model I/O
+   cout << "fIRVersion: " << model.ir_version() << endl;
+   cout << "fModelVersion:" << model.model_version() << endl;
+   cout << "opsetid_size: " << model.opset_import_size() << endl;
+   const onnx::OperatorSetIdProto& opset = model.opset_import(0);
+   cout << "Opset version: " << opset.version() << endl;
 
-  const onnx::GraphProto& graph = model.graph();
-  cout << graph.name() << endl;
-  vector<onnx::NodeProto> node;
-  for (int i=0; i < graph.node_size(); i++){
+   const onnx::GraphProto& graph = model.graph();
+   cout << graph.name() << endl;
+   /*
+   vector<const onnx::NodeProto> node;
+   for (int i=0; i < graph.node_size(); i++){
      node.push_back(graph.node(i));
-  }
-  for (int i=0; i < graph.node_size(); i++){
+   }
+   for (int i=0; i < graph.node_size(); i++){
      cout << node[i].op_type() << endl;
-  }
+   }
+   for (int i=0; i < graph.node_size(); i++){
+     cout << node[i].op_type() << endl;
+   }
+   */
+   std::map<string, std::int64_t> datanode_edge;
+   //size_t will be the index of the other node (send/receive) of the datanode edge
+   std::map<std::int64_t, vector<std::int64_t>> EdgesForward;
+   std::map<std::int64_t, vector<std::int64_t>> EdgesBackward;
+
+
+   for (int i=0; i < graph.input_size(); i++){
+      cout << graph.input(i).name() << endl;
+      datanode_edge[graph.input(i).name()] = -1;
+   }
+   for (int i=0; i < graph.output_size(); i++){
+      cout << graph.input(i).name() << endl;
+      datanode_edge[graph.output(i).name()] = -1;
+   }
+
+   cout << "datanode_edge after initialization" << endl;
+   for (auto const& item: datanode_edge){
+      cout << item.first << ":" << item.second << endl;
+   }
+
+   for (int i=0; i< graph.node_size(); i++){
+      for (int j=0; j < graph.node(i).input_size(); j++){
+         const string& datanode_name {graph.node(i).input(j)};
+         if(datanode_edge.find(datanode_name) != datanode_edge.end()){
+            EdgesForward[datanode_edge[datanode_name]].push_back(i);
+            EdgesBackward[i].push_back(datanode_edge[datanode_name]);
+         }else{
+            datanode_edge[datanode_name] = i;
+         }
+      }
+
+      for (int j=0; j < graph.node(i).output_size(); j++){
+         const string& datanode_name {graph.node(i).output(j)};
+         if(datanode_edge.find(datanode_name) != datanode_edge.end()){
+            EdgesBackward[datanode_edge[datanode_name]].push_back(i);
+            EdgesForward[i].push_back(datanode_edge[datanode_name]);
+         }else{
+            datanode_edge[datanode_name] = i;
+         }
+      }
+   }
+
+   cout << "EdgesForward" << endl;
+   for (auto const& item : EdgesForward){
+      cout << item.first << ":" << print(item.second, graph) << endl;
+   }
+   cout << "EdgesBackward" << endl;
+   for (auto const& item : EdgesBackward){
+      cout << item.first << ":" << print(item.second, graph) << endl;
+   }
 
 
 
@@ -152,7 +223,6 @@ int main(){
 
 
 
-
-  google::protobuf::ShutdownProtobufLibrary();
-  return 0;
+   google::protobuf::ShutdownProtobufLibrary();
+   return 0;
 }
--- generateSimpleAdd.py
+++ generateSimpleAdd.py
@@ -5,8 +5,6 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5)
-        self.conv2 = nn.Conv2d(20, 20, 5)
 
     def forward(self, x, y):
        return x+y
